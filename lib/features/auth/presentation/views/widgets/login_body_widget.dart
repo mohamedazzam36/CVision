f990dec73f32d2b814607a1e176c1dbf3f3f521a
mohamedazzam36 <memeazzam36@gmail.com>
2025-08-22 23:55:49 +0300

    make some refactor

--- lib/features/auth/presentation/views/widgets/login_body_widget.dart
+++ lib/features/auth/presentation/views/widgets/login_body_widget.dart
@@ -1,11 +1,8 @@
 import 'package:cvision/core/utils/app_images.dart';
-import 'package:cvision/features/auth/presentation/views/widgets/custom_auth_button.dart';
-import 'package:cvision/features/auth/presentation/views/widgets/custom_auth_text_field_section.dart';
-import 'package:cvision/features/auth/presentation/views/widgets/do_not_have_account_section.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/login_and_register_body_background_container.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/login_header_section.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/login_method_button.dart';
-import 'package:cvision/features/auth/presentation/views/widgets/remember_me_and_forget_password_section.dart';
+import 'package:cvision/features/auth/presentation/views/widgets/login_user_input_section.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/simple_or_section.dart';
 import 'package:flutter/material.dart';
 
@@ -23,33 +20,7 @@ class LoginBodyWidget extends StatelessWidget {
             const SizedBox(
               height: 16,
             ),
-            const CustomAuthTextFieldSection(
-              title: 'Email',
-              hintText: 'Enter Your Email',
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const CustomAuthTextFieldSection(
-              title: 'Password',
-              hintText: 'Enter Your Password',
-              isPassword: true,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            const RememberMeAndForgetPasswordSection(),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomAuthButton(
-              text: 'Login',
-              onPressed: () {},
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const DoNotHaveAccountSection(),
+            const LoginUserInputSection(),
             const SimpleOrSection(),
             LoginMethodButton(
               iconPath: Assets.imagesGoogleIcon,
