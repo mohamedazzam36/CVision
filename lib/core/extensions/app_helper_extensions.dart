a7586a71797accf382466bf7d95078afdd0257cd
mohamedazzam36 <memeazzam36@gmail.com>
2025-08-22 09:24:23 +0300

    add navigator extension on context

--- lib/core/extensions/app_helper_extensions.dart
+++ lib/core/extensions/app_helper_extensions.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-extension ScreenSizw on BuildContext {
+extension ContextHelper on BuildContext {
   double get width {
     return MediaQuery.sizeOf(this).width;
   }
 
   double get height {
     return MediaQuery.sizeOf(this).height;
+  }
+
+  void navigate(Widget screen) {
+    Navigator.push(
+      this,
+      MaterialPageRoute(
+        builder: (context) => screen,
+      ),
+    );
   }
 }
--- lib/features/auth/presentation/views/widgets/auth_body_widget.dart
+++ lib/features/auth/presentation/views/widgets/auth_body_widget.dart
@@ -1,4 +1,7 @@
+import 'package:cvision/core/extensions/app_helper_extensions.dart';
 import 'package:cvision/core/utils/app_colors.dart';
+import 'package:cvision/features/auth/presentation/views/login_view.dart';
+import 'package:cvision/features/auth/presentation/views/register_view.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/custom_auth_button.dart';
 import 'package:flutter/material.dart';
 
@@ -12,14 +15,14 @@ class AuthBodyWidget extends StatelessWidget {
         CustomAuthButton(
           text: 'Login',
           backgroundColor: AppColors.primaryColor,
-          onPressed: () {},
+          onPressed: () => context.navigate(const LoginView()),
         ),
         const SizedBox(height: 16),
         CustomAuthButton(
           text: 'Create Account',
           textColor: AppColors.primaryColor,
           backgroundColor: AppColors.secondaryColor,
-          onPressed: () {},
+          onPressed: () => context.navigate(const RegisterView()),
         ),
       ],
     );
