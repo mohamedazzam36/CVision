a0ce294952a040113bafea886165f3ad046746e6
mohamedazzam36 <memeazzam36@gmail.com>
2025-08-27 16:16:39 +0300

    finish create accout logic and add navigation extention on buildcontext

--- lib/features/auth/presentation/views/widgets/auth_widgets/auth_successful_body.dart
+++ lib/features/auth/presentation/views/widgets/auth_widgets/auth_successful_body.dart
@@ -1,4 +1,3 @@
-import 'package:cvision/core/extensions/app_helper_extensions.dart';
 import 'package:cvision/core/utils/app_images.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/auth_widgets/auth_body_background_container.dart';
 import 'package:cvision/features/auth/presentation/views/widgets/auth_widgets/auth_successful_body_header.dart';
@@ -7,7 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class AuthSuccessfulBody extends StatelessWidget {
-  const AuthSuccessfulBody({super.key});
+  const AuthSuccessfulBody({
+    super.key,
+    required this.onPressed,
+    required this.title,
+    required this.subTitle,
+    this.secSubTitle,
+  });
+  final String title, subTitle;
+  final void Function() onPressed;
+  final String? secSubTitle;
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +32,17 @@ class AuthSuccessfulBody extends StatelessWidget {
           const SizedBox(
             height: 16,
           ),
-          const AuthSuccessfulBodyHeader(
-            title: 'Sucessful',
-            subTitle: 'Congratulations! Your password has\nbeen changed. Click continue to login',
+          AuthSuccessfulBodyHeader(
+            title: title,
+            subTitle: subTitle,
+            secSubTitle: secSubTitle,
           ),
           const SizedBox(
             height: 56,
           ),
           CustomAuthButton(
             text: 'Continue',
-            onPressed: () {
-              context.popTimes(2);
-            },
+            onPressed: onPressed,
           ),
         ],
       ),
